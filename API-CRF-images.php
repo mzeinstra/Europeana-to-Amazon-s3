737e67d305a66417e135a4069991627549c5e6cd
Maarten Zeinstra <mz@kl.nl>
2015-06-18 13:25:47 +0200

    Updated to export JSON en reduce # of calls to Europeana

--- API-CRF-images.php
+++ API-CRF-images.php
@@ -4,62 +4,102 @@
  */
 ob_start();
 //set output as plain text, for easy copying.
-header('Content-Type: text/plain');
+header('Content-type: text/javascript');
 error_reporting(E_ALL);
 ini_set('display_errors','On');
 
 set_time_limit(0);
 ini_set("allow_url_fopen", true);
 
-$base   = 'http://busy-api.de.a9sapp.eu/v2/'; // host name of the API
-$key    = 'xxx'; // api key
-//+provider_aggregation_edm_isShownBy:*+OR+provider_aggregation_edm_hasView:*'
+
+
+$base   = 'http://beta.europeana.eu/v2/'; // host name of the API
+$key = "XXX";
+
 $query  = '*:*'; // search query
-$sizes  = array('medium','large','extra_large'); // images sizes you want to return (small, medium, large, extra_large)
-$licenses = array("*mark*", "*zero*", "*by\/*", "*by-sa\/*", "*by-nc-sa\/*", "*by-nc\/*");
-$total  = 1000000; // has to be dividable by 50, this number determines how much objects you want to retrieve (media items may be more!)
+$total  = 50; // has to be dividable by 50, this number determines how much objects you want to retrieve (media items may be more!)
 
 // Image sizes
+$sizes  = array('medium','large','extra_large'); // images sizes you want to return (small, medium, large, extra_large)
 $imageSizes = '';
 foreach ($sizes as $size) {
     $imageSizes .= '&qf=IMAGE_SIZE:' . $size;
 }
 
-// Rights
-$rightsQuery = '';
+// Licenses
+$licenses = array("*mark*", "*zero*", "*by/*", "*by-sa/*"); // , "*by-nc-sa\/*", "*by-nc\/*"
+$licenseQuery = '';
 foreach ($licenses as $license) {
-    $rightsQuery .= '&RIGHTS:"' . $license . '"';
+    $licenseQuery .= '&qf=RIGHTS:' . $license;
 }
-$rightsQuery = "(RIGHTS:*mark*+OR+RIGHTS:*zero*+OR+RIGHTS:*by\/*+OR+RIGHTS:*by-sa\/*+OR+RIGHTS:*by-nc-sa\/*+OR+RIGHTS:*by-nc\/*)";
 
-echo $base . 'search.json?start=0&rows=50&query='.$query.$imageSizes.$rightsQuery.'&wskey=' . $key;
-exit;
+$metadataSet = array();
 // Construct search query, we're sending out new requests per 50 objects
-for ($x=0; $x<($total+1); $x+=50) {
-    $result = file_get_contents($base . 'search.json?start='.$x.'&rows=50&query='.$query.$imageSizes.$rightsQuery.'&wskey=' . $key);
+for ($x=1; $x<=$total; $x+=50) {
+	$request = $base . 'search.json?start='.$x.'&rows=50&query='.$query.$imageSizes.'&wskey=' . $key . "&profile=portal+rich"; //&facet=description&facet=who
+    $result = file_get_contents($request);
+
     // Iterate through results
     $results = json_decode($result, true);
     foreach ($results['items'] as $item) {
 
-        // For each object, we have to do a record call to retrieve the direct link to the media
-        $record = json_decode(file_get_contents($base . 'record/'.$item['id'].'.json?wskey='.$key), true);
-        // First, find each webResource that has a mimeType, which means its either the edmIsShownBy or hasView, and resolves to a media object
-        if (isset($record['object']['aggregations'][0]['webResources'])) {
-            foreach ($record['object']['aggregations'][0]['webResources'] as $webResource) {
-                if (isset($webResource['ebucoreHasMimeType'])) {
+		$id = "";
+		$institution = "";
+		$institution_link = "";
+		$file_link = "";
+		$license = "";
+		$source = "";
+		$title = "";
+		$creator = "";
+		$description = "";
+		
+		if (array_key_exists('id',$item)) {
+			$id = $item['id'];
+		} 
+		if (array_key_exists('dataProvider',$item)) {
+			$institution = $item["dataProvider"][0];
+		} 
+		if (array_key_exists('edmIsShownAt',$item)) {
+			$institution_link = $item["edmIsShownAt"][0];
+		} 
+		if (array_key_exists('edmIsShownBy',$item)) {
+			$file_link =  $item['edmIsShownBy'][0];
+		} 
+		if (array_key_exists('rights',$item)) {
+			$license = $item['rights'][0];
+		} 
 
-                    // This webresource is a media from the object..
-					
-                    // ID [europeanaAggregation][about]
-                    echo $record['object']['about']."\t";
-                    // license
-                    echo $record['object']['aggregations'][0]['edmRights']['def'][0]."\t";
-					// Echo the URL to it
-                    echo $webResource['about']."\n";
-                }
-            }
-            ob_flush();
-        }
+		$source = "http://www.europeana.eu/portal/record/" . $id . ".html";
+		
+		if (array_key_exists('id',$item)) {
+			$title = $item['title'][0];
+		} 
+		if (array_key_exists('dcCreator',$item)) {
+			$creator = $item["dcCreator"][0];
+		} 
+		if (array_key_exists('dcDescription',$item)) {
+			$description = $item["dcDescription"][0];
+		} 
+    	//var_dump($item);exit;
+    	$metadataSet[] = array("id" => $id, 
+    							"institution" => $institution, 
+    							"institution_link" => $institution_link, 
+    							"file_link" => $file_link, 
+    							"license" => $license, 
+    							"source" => $source, 
+    							"title" => $title, 
+    							"creator" => $creator, 
+    							"description" => $description);
     }
+    
 }
-ob_end_flush(); 
+$metadataSet = json_encode($metadataSet);
+
+echo $metadataSet;
+
+
+function cleanText($text) {
+	return preg_replace( "/\r|\n|\t/", "", $text );
+}
+
+?>
